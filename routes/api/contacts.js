157c7429d1a166b0f0431f65aa853b0a07e10a3d
VladM <vdstories20@gmail.com>
2023-11-13 02:54:54 +0100

    make task

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,56 @@
-const express = require('express')
+import express from "express";
+import * as contactDate from "../../models/contacts/contacts.js";
+import { HttpError } from "../../helpers/index.js";
+import { contactAddSchema } from "../../validation/schemas.js";
 
-const router = express.Router()
+const contactsRouter = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+contactsRouter.get("/", async (req, res, next) => {
+  try {
+    const result = await contactDate.listContacts();
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error server",
+    });
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+contactsRouter.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contactDate.getContactById(contactId);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    if (!result) {
+      throw HttpError(404, `Contact with id=${contactId} not found`);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+contactsRouter.post("/", async (req, res, next) => {
+  try {
+    const { error } = contactAddSchema.validate(req.body);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await contactDate.addContact(req.body);
+    res.status(201).json(result);
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+contactsRouter.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+contactsRouter.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+export default contactsRouter;
