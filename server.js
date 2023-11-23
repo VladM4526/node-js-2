764fd94d68335d6ca3856dbd6d20c0651f1ce365
VladM <vdstories20@gmail.com>
2023-11-23 14:46:38 +0100

    make task

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+import mongoose from "mongoose";
+
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST_HOMEWORK =
+  "mongodb+srv://VladM20:OD8vDENoD2i4MfP3@vladm20.uhmkyg2.mongodb.net/";
+
+const PORT = 3000;
+
+mongoose
+  .connect(DB_HOST_HOMEWORK)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
