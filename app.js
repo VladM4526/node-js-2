ff98b2b7885415aeb0c1c8bb7b2cf738a9523b29
VladM <vdstories20@gmail.com>
2023-12-01 15:10:55 +0100

    make task

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 import "dotenv/config";
+
+import authRouter from "./routes/api/auth-routers.js";
 import contactRouter from "./routes/api/contactRouters.js";
 
 const app = express();
@@ -12,6 +14,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactRouter);
 
 app.use((req, res) => {
