fc8f32c24da552a7d40ae812cc47dcd9706ed6cf
VladM <vdstories20@gmail.com>
2023-12-03 18:34:16 +0100

    error status 401

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -12,7 +12,7 @@ const { JWT_SECRET } = process.env;
 const signup = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
-  if (!user) {
+  if (user) {
     throw HttpError(409, "Email already exist");
   }
 
@@ -42,6 +42,7 @@ const signin = async (req, res) => {
   };
 
   const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, { token });
 
   res.json({
     token,
@@ -70,5 +71,5 @@ export default {
   signup: ctrlWrapper(signup),
   signin: ctrlWrapper(signin),
   getCurrent: ctrlWrapper(getCurrent),
-  signout: ctrlWrapper(signout)
+  signout: ctrlWrapper(signout),
 };
